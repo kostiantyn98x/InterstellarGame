8f09172aed505a64d72e10ebac96ce65cc51e452
Future-2023 <127534411+kostiantyn98x@users.noreply.github.com>
2022-09-07 02:34:32 +0200

    complete UI

--- src/lib/config.js
+++ src/lib/config.js
@@ -9,19 +9,4 @@ export const CONFIG = {
     tradingPoolID: '0xefa3180c74b886f87b3ad7c1c801c59b7ce825cd',
 
     nullAddress: '0x0000000000000000000000000000000000000000',
-
-    eth_binance_api: 'https://api.binance.com/api/v3/ticker/24hr?symbol=ETHUSDT',
-    bnb_binance_api: 'https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT',
-
-    // Main default setting parameter
-    MainDecimal: 9,
-    defaultSlippagePct: 1,
-    tradingFee: 1, // this is % value
-    TLPPrice: 1, // this is % value
-    TRYPrice: 1,
-    faucetDurationTime: 10 * 60 * 1000, // 1 hour
-    timeIntervalOfPrice: 300,    
-    timeIntervalOfReward: 300,    
-    referCodeLength: 100000000000,
-    TLPDecimal: 100000
 }

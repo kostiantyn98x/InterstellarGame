dacbd97ae3e948d32399f58b15cdc3526c507cc6
Future-2023 <127534411+kostiantyn98x@users.noreply.github.com>
2022-09-07 02:36:23 +0200

    last fixed in Server

--- src/control/importModule.js
+++ src/control/importModule.js
@@ -4,3 +4,71 @@ export const importImage = (symbol) => {
     value = '../../img/png/eth-bg.png';
   } 
 }
+
+export class Coin {
+  typeArg
+  id
+  balance
+
+  constructor(typeArg, id, balance) {
+      this.typeArg = typeArg
+      this.id = id
+      this.balance = new Balance(typeArg, balance)
+  }
+}
+
+export const getMainCoins = (tokenPrice, lpPool) => {
+const mainCoinList = ['SUI', 'ETH', 'BTC'];
+let mainCoin = [];
+mainCoinList.map(item => {
+
+  let price = 0;
+  let changeValue = 0;
+  let isEarn = 0;
+  let tokenName = getTokenName(item);
+  let tokenIcon = importImage(item);
+  let tokenId = undefined;
+
+  lpPool.map(itemValue => {
+    if(itemValue.metadata[0].symbol == item) {
+      tokenPrice.map(item => {
+        if(item.symbol == itemValue.metadata[0].symbol) {
+          price = item.value;
+          changeValue = item.changeValue;
+          isEarn = item.isEarn;
+        }
+      })
+      tokenId = itemValue.metadata[0].typeArg;
+      // price = Number(itemValue.data.balanceB.value) / Number(itemValue.data.balanceA.value);
+    }
+  })
+  let value = {
+    symbol: item,
+    tokenId: tokenId,
+    price: Number(price).toFixed(3),
+    tokenName: tokenName,
+    tokenIcon: tokenIcon,      
+    changeValue: changeValue,
+    isEarn: isEarn,
+    label: item,
+  }
+  mainCoin.push(value);
+})
+return mainCoin;
+}
+  
+export const getSwapPrice = (inPool, outPool, value) => {
+let bigIntAmount = changeBigNumber(value);
+let USDAmount = calcSwapOut(inPool, bigIntAmount, true);
+let SecondTokenAmount = calcSwapOut(outPool, USDAmount, false);
+return changeDecimal8Fix(SecondTokenAmount);
+}
+
+export const isLoggedIn = () => {
+  const account = localStorage.getItem('walletAddress');
+  if(account == undefined || account == "null" || account == "undefined") {
+    return false;
+  } else {
+    return true;
+  }
+}
--- src/hooks/useBalance.js
+++ src/hooks/useBalance.js
@@ -3,13 +3,4 @@ import Big from 'big.js';
 
 export const useBalance = (account) => {
     const [balance, setBalance] = useState('0.00');
-  
-    
-    useEffect(() => {
-      getBalance();
-    }, [account]);
-  
-    return {
-      balance
-    }
 }
